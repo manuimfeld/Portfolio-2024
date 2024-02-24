da8f35aef3f3f03fdc5b5c1f0a8d53ff00cb7421
manuimfeld <manu_jadir@hotmail.com>
2024-02-24 00:31:03 -0300

    add styles css and information

--- src/app/ui/aboutMe.js
+++ src/app/ui/aboutMe.js
@@ -1,17 +1,38 @@
 export default function AboutMe() {
   return (
-    <section className="h-[calc(100vh_-_64px)] w-full">
-      <h2 className="text-black">Quién soy</h2>
-      <p>
-        "Hola a todxs! 🌈 Para los que no me conocen aún, mi nombre es Johanna y
-        quiero compartirles un poco sobre mí 🌙✨
-      </p>
-      <article className="relative  w-full h-full bg-[url('/bg-aboutme2.jpg')] bg-cover bg-fixed ">
-        <div class="absolute inset-0 bg-blue-300 opacity-20"></div>
-        <div class="relative z-10">
-          <h2 className="text-4xl font-bold">Quien soy</h2>
-          <p>Y esot es una pagina</p>
-        </div>
+    <section
+      id="sobre-mi"
+      className="relative py-6 text-center min-h-full md:h-[calc(100vh_-_64px)] w-full bg-[url('/bg-aboutme5.jpg')] bg-cover bg-fixed"
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <h1 className="text-3xl relative z-10 w-text-3xl py-1 text-primaryLighter font-bold uppercase">
+        Quién soy
+      </h1>
+      <article className="md:flex md:items-center text-secondary relative z-10 flex flex-col items-center px-5 py-4 w-full min-h-full  ">
+        <p className="text-lg mt-3">Hola a todxs! 🌈</p>
+        <p className="md:w-3/6 text-lg mb-3">
+          Soy Moona, aunque mi nombre real es Joha soy Astróloga y Tarotista🌙✨
+        </p>
+        <p className="md:w-3/6 text-lg my-3">
+          Hace más de 5 años que me dedico a encontrar(me) y gracias a eso,
+          ayudar a otrxs a verse a través de las{" "}
+          <span className="font-bold text-white">herramientas esotéricas</span>.
+        </p>
+        <span className="text-3xl leading-none">°</span>
+        <p className="md:w-3/6 text-lg my-3">
+          Mi verdadera vocación está relacionada con el autoconocimiento
+          terapéutico y la exploración interna.
+        </p>
+        <p className="md:w-3/6 text-lg my-3">
+          Mi espacio brinda las oportunidades para que te conozcas en
+          profundidad y puedas potenciar tus dones y talentos innatos.
+        </p>
+        <span className="md:w-3/6 text-3xl leading-none">°</span>
+
+        <p className="text-lg my-3">
+          Busquemos juntxs ese brillo extra-ordinario que hay en vos a través
+          del conocimiento que nos brinda la magia.
+        </p>
       </article>
     </section>
   );
