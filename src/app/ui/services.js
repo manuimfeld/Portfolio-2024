f8d702661d8365c7ce342d442758fe66282008bc
manuimfeld <manu_jadir@hotmail.com>
2024-02-24 00:31:18 -0300

    add styles css and information

--- src/app/ui/services.js
+++ src/app/ui/services.js
@@ -1,18 +1,30 @@
-import { SERVICES_ARRAY } from '../lib/services';
-import CardServices from './cardServices';
+import { SERVICES_ARRAY } from "../lib/services";
+import CardServices from "./cardServices";
 
 export default function Services() {
   return (
-    <section className="text-center min-w-full min-h-[calc(100vh_-_64px)]">
-      <h1 className="text-3xl text-[#BB9AD1]">Servicios</h1>
-      <p className="mt-2 px-4 text-gray-400 text-xl font-medium">
-        Hay una variedad de herramientas que nos pueden guiar en el viaje de
-        descubrir nuestra verdadera esencia y desatar el poder transformador del
-        universo en la vida. Cada experiencia es única y diseñada para ofrecerte
-        una guía auténtica y significativa en tu búsqueda de autoconocimiento y
-        sanación interior
+    <section
+      id="servicios"
+      className="py-6 px-5 text-center min-w-full min-h-auto bg-white"
+    >
+      <h1 className="md:mx-auto md:w-3/6 text-3xl py-1 text-primary font-bold uppercase">
+        Servicios
+      </h1>
+      <p className="md:mx-auto md:w-3/6 text-lg my-3 text-grayLight">
+        Hay una variedad de
+        <span className="text-grayDark font-bold"> herramientas</span> que nos
+        pueden guiar en el viaje de{" "}
+        <span className="text-grayDark font-bold">descubrir</span> nuestra
+        verdadera <span className="text-grayDark font-bold">esencia</span> y
+        desatar el poder transformador del universo en la vida.
       </p>
-      <div className="mt-4 rounded-[20px] py-5 h-[calc(100vh - 64px)] flex overflow-x-auto justify-start bg-[url('/bg-aboutme4.jpg')] bg-cover bg-center ">
+      <p className="md:mx-auto md:w-3/6 text-lg my-3 text-grayLight">
+        Cada experiencia es{" "}
+        <span className="text-secondary font-bold">única y diseñada</span> para
+        ofrecerte una guía auténtica y significativa en tu búsqueda de
+        autoconocimiento y sanación interior
+      </p>
+      <div className="md:flex md:justify-around mt-4 rounded-[20px] min-h-auto w-full">
         {SERVICES_ARRAY.map((service, index) => {
           return <CardServices key={service.id} service={service} />;
         })}
