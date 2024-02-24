dc3f30d84434a40dcd2a7565cb00c23f32873df2
manuimfeld <manu_jadir@hotmail.com>
2024-02-24 00:30:11 -0300

    change html elements for SEO

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,22 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import AboutMe from './ui/aboutMe';
-import Services from './ui/services';
+import Image from "next/image";
+import Link from "next/link";
+import AboutMe from "./ui/aboutMe";
+import Services from "./ui/services";
 
 export default function Home() {
   return (
     <>
-      <main className="text-center flex flex-col justify-center items-center w-full px-5 min-h-[calc(100vh_-_64px)]">
-        <Image
-          src="/moonatica.png"
-          width="350"
-          height="350"
-          className="opacity-0"
-        />
-        <p className="text-xl text-[#BB9AD1]">Astrología y Tarot inicial</p>
-        <h3 className=" mt-5 text-3xl text-[#BB9AD1]">
+      <main
+        id="inicio"
+        className="text-center flex flex-col justify-center items-center w-full px-5 min-h-[calc(100vh_-_64px)]"
+      >
+        <Image src="/moonatica.png" width="350" height="350" />
+        <h1 className="text-xl text-primaryLight">
+          Astrología y Tarot inicial
+        </h1>
+        <h2 className=" mt-5 text-3xl text-primaryLight">
           Transforma tu vida explorando tu esencia
-        </h3>
+        </h2>
       </main>
       <AboutMe />
       <Services />
